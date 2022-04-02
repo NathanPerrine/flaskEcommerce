2393d87a0eec7b156bde025f4ab2caf5d19c67fa
NathanPerrine <nathan.perrine@gmail.com>
2022-04-02 18:31:54 -0400

    Can add books to cart

---
+++ app/blueprints/shop/models.py
@@ -0,0 +1,12 @@
+from app import db 
+
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'))
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        db.session.add(self)
+        db.session.commit()
+    
--- app/blueprints/shop/routes.py
+++ app/blueprints/shop/routes.py
@@ -1,19 +1,19 @@
 from . import shop 
 from flask import render_template, redirect, url_for, flash
 from flask_login import login_required, current_user
-# from .forms import shop
 from app.blueprints.seller.models import Book
+from app.blueprints.shop.models import Cart
 
 @shop.route('/')
 def index():
     title = 'Home'
     return render_template('index.html', title=title)
 
-@shop.route('/cart')
-def cart():
+@shop.route('/view-cart')
+def view_cart():
     title = "Cart"
     
-    return render_template('cart.html', title=title)
+    return render_template('view_cart.html', title=title)
 
 @shop.route('/store')
 def store():
@@ -29,3 +29,11 @@ def full_item(item_id):
     title = f"{item} Full Info"
 
     return render_template('full_item.html', title = title, item = item)
+
+@shop.route('/add-to-cart/<item_id>')
+@login_required 
+def add_to_cart(item_id):
+    item = Book.query.get_or_404(item_id)
+    new_cart = Cart(user_id = current_user.id, book_id = item.id)
+    flash(f"{item.title} has been added to your cart.", "success")
+    return redirect(url_for('shop.store'))
